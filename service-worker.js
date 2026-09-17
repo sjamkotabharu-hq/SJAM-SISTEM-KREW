684c511f4d37a58d522afebda1af658033ca7a91
sjamkotabharu-hq <sjamkotabharu@gmail.com>
2026-09-18 00:16:01 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,40 @@
-const CACHE_NAME = "sjam-krew-v1";
+const CACHE_NAME = "sjam-krew-v2";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+
+self.skipWaiting();
+
 });
 
+
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+
+caches.keys().then(keys => {
+
+return Promise.all(
+
+keys.map(key => caches.delete(key))
+
+);
+
+})
+
+);
+
+self.clients.claim();
+
+});
+
+
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request))
-  );
+
+event.respondWith(
+
+fetch(event.request)
+
+);
+
 });
