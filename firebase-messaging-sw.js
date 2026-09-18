1f02b319eab49bea1ce1dc3a43a2c125806be477
sjamkotabharu-hq <sjamkotabharu@gmail.com>
2026-09-18 17:27:41 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,10 +6,6 @@ importScripts(
 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js'
 );
 
-importScripts(
-'https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js'
-);
-
 
 // ==============================
 // FIREBASE CONFIG
